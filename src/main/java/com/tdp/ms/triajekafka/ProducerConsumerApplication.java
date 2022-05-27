35e24abec7c16c386ee9ba3ac8f6cd2e246d24bf
GabaRivera <grivera@indracompany.com>
2022-05-27 18:05:31 -0500

    weflux dependecie

--- src/main/java/com/tdp/ms/triajekafka/ProducerConsumerApplication.java
+++ src/main/java/com/tdp/ms/triajekafka/ProducerConsumerApplication.java
@@ -3,10 +3,12 @@ package com.tdp.ms.triajekafka;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
 
 import com.tdp.genesis.core.starter.web.runner.StarterWebApplication;
 
 
+@EnableWebFlux
 @SpringBootApplication
 public class ProducerConsumerApplication extends StarterWebApplication{
 
